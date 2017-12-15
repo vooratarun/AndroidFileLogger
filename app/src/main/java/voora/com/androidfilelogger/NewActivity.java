b3dae8bfa0f55994a76d182e21485bc862eade41
TARUN <vooratarun@gmail.com>
2017-12-15 20:50:44 +0530

    added examples

--- app/src/main/java/voora/com/androidfilelogger/NewActivity.java
+++ app/src/main/java/voora/com/androidfilelogger/NewActivity.java
@@ -1,57 +1,56 @@
 package voora.com.androidfilelogger;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import timber.log.Timber;
 
 
-public class MainActivity extends AppCompatActivity {
+public class NewActivity extends AppCompatActivity {
 
-    private static final String TAG = "Main";
+
+    private static final String TAG = "NewActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_new);
         Timber.d("onCreate");
 
-        findViewById(R.id.helloBtn).setOnClickListener(v -> {
-            startActivity(new Intent(this,NewActivity.class));
-        });
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Timber.d("onPause");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Timber.d("onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Timber.d("onResume");
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Timber.d("onStart");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Timber.d("onPause");
+
+    }
 
     @Override
     protected void onStop() {
         super.onStop();
         Timber.d("onStop");
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Timber.d("onDestroy");
+
     }
-
-
 }
